8d67cbfef51691211d1c68f215790291cb401f83
OmariBennett <omari.bennett@gmail.com>
2021-08-11 17:29:00 -0400

    new todo list with React hooks

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import About from './components/pages/About';
-import Header from './components/layout/Header';
-import TodoAppRouter from './components/todoRouter/Index';
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import About from './components/pages/About';
+// import Header from './components/layout/Header';
 // import TodoApp from './components/todo/Index';
+// import TodoAppRouter from './components/todoRouter/Index';
+import TodoAppHooks from './components/todoHooks/Index';
 
 export default class App extends Component {
 	render() {
 		return (
-			<Router>
-				<div>
-					<Header />
-					{/* <Route exact path='/' component={TodoApp} /> */}
-					<Route exact path='/' component={TodoAppRouter} />
-					<Route path='/about' component={About} />
-				</div>
-			</Router>
+			<div>
+				<TodoAppHooks />
+			</div>
+			// <Router>
+			// 	<div>
+			// 		<Header />
+			// 		<Route exact path='/' component={TodoApp} />
+			// 		<Route exact path='/' component={TodoAppRouter} />
+			// 		<Route path='/about' component={About} />
+			// 	</div>
+			// </Router>
 		);
 	}
 }
---
+++ src/components/todoHooks/Index.js
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+
+function Todo({ index, todo, completeTodo, removeTodo }) {
+	return (
+		<div style={{ textDecoration: todo.isCompleted ? 'line-through' : 'none' }}>
+			<input type='checkbox' onClick={() => completeTodo(index)} />
+			{todo.text}
+			<button onClick={() => removeTodo(index)}>Delete</button>
+		</div>
+	);
+}
+
+function TodoForm({ addTodo }) {
+	const [value, setValue] = useState('');
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (!value) return;
+		addTodo(value);
+		setValue('');
+	};
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<input
+				type='text'
+				value={value}
+				placeholder='Add todo...'
+				onChange={(e) => setValue(e.target.value)}
+			/>
+		</form>
+	);
+}
+
+export default function TodoAppHooks() {
+	const [todos, setTodos] = useState([
+		{
+			text: 'Learn about React',
+			isCompleted: false,
+		},
+		{
+			text: 'Meet friend for lunch',
+			isCompleted: false,
+		},
+		{
+			text: 'Build really cool todo app',
+			isCompleted: false,
+		},
+	]);
+
+	const addTodo = (text) => {
+		const newTodos = [...todos, { text }];
+		setTodos(newTodos);
+	};
+
+	const completeTodo = (index) => {
+		const newTodos = [...todos];
+		newTodos[index].isCompleted = !newTodos[index].isCompleted;
+		setTodos(newTodos);
+	};
+
+	const removeTodo = (index) => {
+		const newTodos = [...todos];
+		newTodos.splice(index, 1);
+		setTodos(newTodos);
+	};
+
+	return (
+		<div>
+			{todos.map((todo, index) => (
+				<Todo
+					key={index}
+					index={index}
+					todo={todo}
+					completeTodo={completeTodo}
+					removeTodo={removeTodo}
+				/>
+			))}
+			<TodoForm addTodo={addTodo} />
+		</div>
+	);
+}
